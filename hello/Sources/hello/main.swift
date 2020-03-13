00c4c6c9bbdcb619e78f0e3601f9134ff7a36393
camus0129 <shenhao0129@gmail.com>
2020-03-13 17:58:17 +0800

    Added func

--- hello/Sources/hello/main.swift
+++ hello/Sources/hello/main.swift
@@ -1,36 +1,44 @@
 let fee=0.9988
 var last_price=1.6310
-var my_share=4459.32
-var my_cost=6950
-var my_cost_price=1.5585
-var my_sum=last_price*my_share
-var my_profit=(last_price-my_cost_price)*my_share
-var my_profit_rate=((last_price-my_cost_price)/my_cost_price)*100
-
-print("my_sum is \(my_sum)")
-print("my_profit is \(my_profit)")
-print("my_profit_rate \(my_profit_rate)%")
-
-var buy=1000
+var current_share=4459.32
+var current_cost=6950.0
+var current_cost_price=1.5585
+var buy=1000.0
 var latest_price=1.6080
-var buy_net=Double(buy)*fee
+var want_profit_rate = 0.01
+
+func current_current_state(){
+	var current_sum=last_price*current_share
+	var current_profit=(last_price-current_cost_price)*current_share
+	var current_profit_rate=((last_price-current_cost_price)/current_cost_price)*100
+	print("current_sum is \(current_sum)")
+	print("current_profit is \(current_profit)")
+	print("current_profit_rate \(current_profit_rate)%")
+}
+
+func target_state(buy:Double,latest_price:Double){
+	var buy_net=Double(buy)*fee
 var buy_share=buy_net/latest_price;
-var target_share=my_share+buy_share
-var target_cost=my_cost+buy
+var target_share=current_share+buy_share
+var target_cost=current_cost+buy
 var target_cost_price=Double(target_cost)/target_share
 var target_profit=(latest_price-target_cost_price)*target_share
 var target_profit_rate=((latest_price-target_cost_price)/target_cost_price)*100
-
 print("target_cost_price is \(target_cost_price)")
 print("target_profit is \(target_profit)")
 print("target_profit_rate is \(target_profit_rate)%")
+}
 
-
-var want_profit_rate = 0.01
-var want_cost_price=latest_price/(want_profit_rate+1)
-var temp1=want_cost_price*my_share-Double(my_cost)
+func need_to_buy(wpr:Double){
+	var want_cost_price=latest_price/(want_profit_rate+1)
+var temp1=want_cost_price*current_share-Double(current_cost)
 var temp2=1-want_cost_price*fee/latest_price
 var want_buy=temp1/temp2
-
 print("need to buy \(want_buy)")
+}
+
+
+current_current_state()
+target_state(buy:buy,latest_price:latest_price)
+need_to_buy(wpr:want_profit_rate)
 
